481b4c2b838dd6d276254a4421f0e50e4bdf85f9
Patrick Chaves <patrick095@gmail.com>
2022-03-26 14:35:30 -0300

    lint

--- src/pipe/validation.pipe.spec.ts
+++ src/pipe/validation.pipe.spec.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from './validation.pipe';
 
 describe('ValidationPipe', () => {
-  it('should be defined', () => {
-    expect(new ValidationPipe()).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(new ValidationPipe()).toBeDefined();
+    });
 });
