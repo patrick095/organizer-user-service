6ffe96c221e9cfda9c198fd336678f6ca091f3c9
Patrick Chaves <patrick095@gmail.com>
2022-03-26 17:13:47 -0300

    add signup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, UsePipes } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { UserDTO } from './dto/userDTO';
 import { UserSigninDTO } from './dto/userSigninDTO';
 import { CustomValidationPipe } from './pipe/validation.pipe';
 
@@ -12,5 +13,11 @@ export class AppController {
     @UsePipes(new CustomValidationPipe())
     signin({ username, password }: UserSigninDTO) {
         return this.appService.signin(username, password);
+    }
+
+    @MessagePattern({ role: 'user', cmd: 'signup' })
+    @UsePipes(new CustomValidationPipe())
+    signup(user: UserDTO) {
+        return this.appService.signup(user);
     }
 }
